8c5d59d230a9e84613d7366be5e3ab6cb93d00cf
Jezebel1990 <jezebelguedes@gmail.com>
2023-09-03 17:50:17 -0300

    feat: integrate routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Route } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '', //rota inicial
+        pathMatch: 'full', //raiz do projeto
+        component: HelloWorldComponent
+    },
+    {
+        path: 'products', //rota pelo navegador
+        loadChildren: () => import('modules/products').then(m => m.ProductsModule)
+    },
+    {
+        path: 'orders',
+        loadChildren: () => import('modules/orders').then(m => m.OrdersModule)
+    }
+];
