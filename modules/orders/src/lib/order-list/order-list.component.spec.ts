998414af3f580dfa85da489049865bf2835a7c84
Jezebel1990 <jezebelguedes@gmail.com>
2023-09-05 17:10:18 -0300

    test: add order list component tests

--- modules/orders/src/lib/order-list/order-list.component.spec.ts
+++ modules/orders/src/lib/order-list/order-list.component.spec.ts
@@ -1,15 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { OrderListComponent } from './order-list.component';
+import { StarRatingComponent } from 'modules/shared/ui';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RatingService } from '../services/rating/rating.service';
 
 describe('OrderListComponent', () => {
   let component: OrderListComponent;
   let fixture: ComponentFixture<OrderListComponent>;
+  let ratingService: RatingService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [OrderListComponent],
+      //schemas: [NO_ERRORS_SCHEMA],
+      imports: [StarRatingComponent, ReactiveFormsModule],
     }).compileComponents();
 
+    ratingService = TestBed.inject(RatingService);
     fixture = TestBed.createComponent(OrderListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +24,53 @@ describe('OrderListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should enable button when form is valid', () => {
+    const button: HTMLButtonElement =
+      fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBe(true);
+
+    const [firstStar]: HTMLSpanElement[] =
+      fixture.nativeElement.querySelectorAll('[role=radio]');
+    firstStar.click();
+
+    fixture.detectChanges();
+
+    expect(button.disabled).toBe(false);
+  });
+
+  it('should call "onSubmit" function when button is clicked', () => {
+    jest.spyOn(component, 'onSubmit');
+    const button: HTMLButtonElement =
+      fixture.nativeElement.querySelector('button');
+
+    const [firstStar]: HTMLSpanElement[] =
+      fixture.nativeElement.querySelectorAll('[role=radio]');
+    firstStar.click();
+
+    fixture.detectChanges();
+
+    button.click();
+
+    expect(component.onSubmit).toHaveBeenCalledTimes(1);
+  });
+  it('should call "setRating" function when button is clicked', () => {
+    jest.spyOn(ratingService, 'setRating');
+    const button: HTMLButtonElement =
+      fixture.nativeElement.querySelector('button');
+
+    const [firstStar, secondStar]: HTMLSpanElement[] =
+      fixture.nativeElement.querySelectorAll('[role=radio]');
+    secondStar.click();
+
+    fixture.detectChanges();
+
+    button.click();
+
+    expect(ratingService.setRating).toHaveBeenCalledWith({
+      rating: 2,
+      comment: '',
+    });
   });
 });
